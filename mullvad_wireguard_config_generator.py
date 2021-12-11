6c85663953cc5e6fbbc4982e4b79ebbc70de2118
12932 <68835423+12932@users.noreply.github.com>
2021-12-11 13:57:44 +0800

    Update mullvad_wireguard_config_generator.py

--- mullvad_wireguard_config_generator.py
+++ mullvad_wireguard_config_generator.py
@@ -18,24 +18,56 @@ for the config files are better
 import pathlib
 import requests
 import os
+import sys
 
 MULLVAD_SERVER_LIST_ENDPOINT = "https://api.mullvad.net/www/relays/all/"
-ADDRESS = "10.0.0.1/32,fc00:bbbb:bbbb:bbbb::5:5/128"
-PRIVATE_KEY = "IDfT7VrnvBWxX+XOk8DO0mo6VT2D3IJDsgAmMuCOGWs="
+ADDRESS = "10.555.555.555/32,fc00:bbbb:bbbb:bb01::5:511e/128"
+PRIVATE_KEY = "PUT YOUR PRIVATE KEY HERE"
 DNS = "1.1.1.1"
 FILE_ILLEGAL_CHARS = r"/?:\<>*|#, "
 
 CONFIG_DIRECTORY = f"{pathlib.Path().absolute()}\MullvadConfigs"
 
-def sanitise_string(text): #removes all characters that are illegal in windows filenames
+import pprint
+# A filter that returns unique providers for each city
+# gives the server with the highest number
+# Returns a list of servers
+def filter_newest_unique_provider(server_data):
+	server_with_numbers = []
+	filtered_servers = {}
+
+	#There *has* to be a better way to do this
+	for server in server_data:
+		country_code = server.get("country_code")
+		city_server_number = server.get("hostname").replace("-wireguard", "").replace(country_code, "")
+		server["city_server_number"] = city_server_number
+		server_with_numbers.append(server)
+
+	for server in server_with_numbers:
+		server_num = 0
+		server_city_name = server["city_name"]
+		server_provider = server["provider"]
+		server_string = f"{server_city_name}-{server_provider}"
+		if server_string in filtered_servers:
+			server_num = int(filtered_servers[server_string]["city_server_number"])
+		
+		if int(server["city_server_number"]) > server_num:
+			filtered_servers[server_string] = server
+	
+	return list(filtered_servers.values())
+
+# removes all characters that are illegal in windows filenames
+def sanitise_string(text): 
 	return text.translate({ord(c): None for c in FILE_ILLEGAL_CHARS})
 
-def remove_all_files_in_directory(directory): #removes all FILES in a given directory
+# removes all FILES in a given directory
+def remove_all_files_in_directory(directory): 
 	for root, dirs, files in os.walk(directory):
 		for file in files:
 			os.remove(os.path.join(root, file))
 
-def save_config_to_file(jsondata): #given a server json, make a config file from it
+# given a server json, make a config file from it
+def save_config_to_file(jsondata): 
 	servername = jsondata.get('hostname').replace("-wireguard", "")
 	#city_code = jsondata.get('city_code')
 	city_code = jsondata.get('city_name')
@@ -48,7 +80,8 @@ def save_config_to_file(jsondata): #given a server json, make a config file from
 	with open(f"{CONFIG_DIRECTORY}\{filename}", "w", encoding="utf-8") as outfile:
 		outfile.write(f"{configstring}")
 
-def generate_wireguard_config(jsondata): #generates a wireguard config string, given a server json
+# generates a wireguard config string, given a server json
+def generate_wireguard_config(jsondata): 
 	configstring = "[Interface]\n"
 	configstring += f"PrivateKey = {PRIVATE_KEY}\n"
 	configstring += f"Address = {ADDRESS}\n"
@@ -62,18 +95,22 @@ def generate_wireguard_config(jsondata): #generates a wireguard config string, g
 
 if __name__ == "__main__":
 	pathlib.Path(f"{CONFIG_DIRECTORY}").mkdir(parents=True, exist_ok=True)
-	remove_all_files_in_directory(CONFIG_DIRECTORY) #remove all old config files
+	# remove all old config files
+	remove_all_files_in_directory(CONFIG_DIRECTORY) 
 
 	server_data_request = requests.get(MULLVAD_SERVER_LIST_ENDPOINT, timeout=(11,30))
 	server_data_request.raise_for_status()
 
 	server_data = server_data_request.json()
 
-	for item in server_data:
-		server_type = item.get("type")
+	wireguard_servers = [server for server in server_data if server.get("type") == "wireguard"]
+	
+	# Filter out to unique providers/city only
+	#filtered_servers = filter_newest_unique_provider(wireguard_servers)
+	filtered_servers = wireguard_servers
 
-		if server_type == "wireguard":
-			#print(item)
-			save_config_to_file(item)
+	#print(wireguard_servers)
+	for server_item in filtered_servers:
+		save_config_to_file(server_item)
 
-	print(f"Saved {len(server_data)} config files to {CONFIG_DIRECTORY}")
+	print(f"Saved {len(filtered_servers)} config files to {CONFIG_DIRECTORY}")
